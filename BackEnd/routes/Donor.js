a94fdc25c0d7fdcbd0c81b2c6c830dd80b27f5b5
Naziya13 <60518581+Naziya13@users.noreply.github.com>
2021-05-30 12:15:38 +0530

    Update Donor.js

--- BackEnd/routes/Donor.js
+++ BackEnd/routes/Donor.js
@@ -8,13 +8,10 @@ var multerS3 = require('multer-s3');
 var awsconfig = {
   "region": "ap-south-1",
   "endpoint": "http://dynamodb.ap-south-1.amazonaws.com",
-<<<<<<< HEAD
+
  "accessKeyId":'',
 "secretAccessKey":''
-=======
-  "accessKeyId": 'AKIATSPZDOCGCYECV3G7',
-  "secretAccessKey": '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'
->>>>>>> 8fcd2400d6c59a92628b504924f2548d00d2e2d8
+
 }
 
 AWS.config.update(awsconfig)
@@ -55,13 +52,12 @@ var upload = multer({ storage: storage }, { dest: 'uploads/' });
 const s3 = new AWS.S3({
   "region": "ap-south-1",
   "endpoint": "http://s3.ap-south-1.amazonaws.com",
-<<<<<<< HEAD
+
  "accessKeyId":'',
 "secretAccessKey":''
-=======
-  "accessKeyId": 'AKIATSPZDOCGCYECV3G7',
-  "secretAccessKey": '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'
->>>>>>> 8fcd2400d6c59a92628b504924f2548d00d2e2d8
+
+ 
+
 });
 
 
