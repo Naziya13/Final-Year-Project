7842ad95fe0ff10b83aea6a1e96b6102ce671e48
Naziya13 <60518581+Naziya13@users.noreply.github.com>
2021-05-30 12:44:05 +0530

    Update Thankyou.js

--- BackEnd/routes/Thankyou.js
+++ BackEnd/routes/Thankyou.js
@@ -5,8 +5,8 @@ var AWS = require('aws-sdk');
 var awsconfig = {
     "region": "ap-south-1",
     "endpoint": "http://dynamodb.ap-south-1.amazonaws.com",
-    "accessKeyId": 'AKIA25SDQSDGHID7RQHH',
-    "secretAccessKey": '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'
+    "accessKeyId":'',
+
 }
 
 AWS.config.update(awsconfig)
